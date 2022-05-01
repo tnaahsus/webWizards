44d6a352d80ed67045994824e895ea36c583003d
tnaahsus <72088338+tnaahsus@users.noreply.github.com>
2022-05-01 22:48:02 +0530

    pointer change

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function canvas() {
     zoom *= 0.999 ** delta;
     if (zoom > 20) zoom = 20;
     if (zoom < 1) zoom = 1;
-    canvas.setZoom(zoom);
+    canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
     opt.e.preventDefault();
     opt.e.stopPropagation();
   });
